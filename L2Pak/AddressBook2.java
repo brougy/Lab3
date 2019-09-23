24f9a0168e8dd50457e0e8cac030f425601f1de3
brougyelberougy <brougyelberougy@cb5109-52.labs.sce.carleton.ca>
2019-09-23 12:16:33 -0400

    Test Commit

---
+++ L2Pak/AddressBook2.java
@@ -0,0 +1,45 @@
+package L2Pak;
+
+import java.util.ArrayList;
+
+public class AddressBook2 {
+	
+	private ArrayList<BuddyInfo2> list;
+	
+	public AddressBook2() {
+		this.list = new ArrayList<BuddyInfo2>();
+	}
+
+	public static void main(String[] args) {
+	/*	// TODO Auto-generated method stub
+		BuddyInfo2 person = new BuddyInfo2("Sandeep", "613-979-8000", "12 Liberty Court");
+		BuddyInfo2 person2 = new BuddyInfo2("John", "647-400-3780", "57 Theodore Drive");
+		BuddyInfo2 person3 = new BuddyInfo2("Sandeep", "613-979-8000", "12 Liberty Court");
+		AddressBook2 buddy = new AddressBook2();
+		buddy.addBuddy(person);
+		buddy.addBuddy(person2);
+		buddy.removeBuddy(person3);
+		System.out.println(buddy.toString());
+		*/
+		
+		System.out.println("Address Book");
+		
+	}
+	
+	public void addBuddy(BuddyInfo2 person) {
+		this.list.add(person);
+	}
+	
+	public void removeBuddy(BuddyInfo2 person) {
+		this.list.remove(person);
+	}
+	
+	public String toString() {
+		String s = new String();
+		for (BuddyInfo2 b : list) {
+			s += b.getName();
+		}
+		return s;
+	}
+
+}
